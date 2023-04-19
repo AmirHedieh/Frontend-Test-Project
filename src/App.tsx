eee08b3f6088b78c5567e95928d110213b1d5db7
AmirHedieh <amirhosseinhediehloo@gmail.com>
2023-04-20 00:42:28 +0330

    WIP: update App component and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import LoginScene from './scenes/login_scene/LoginScene'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+export const App: React.FC = () => {
+  return <LoginScene />
 }
-
-export default App
