eb58a1321d1f431c5b56e4f98454c9c2fe7f5f22
AmirHedieh <amirhosseinhediehloo@gmail.com>
2023-04-21 20:28:07 +0330

    Add GlobalStyles file

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,25 @@
+export const FontSizes = {
+  /** 30 */
+  h0: 30,
+  /** 24 */
+  h1: 24,
+  /** 20 */
+  h2: 20,
+  /** 16 */
+  h3: 16,
+  /** 14 */
+  p: 14,
+  /** 12 */
+  extraSmall: 12,
+}
+
+export const Fonts = {
+  persian: {
+    vazir: 'Vazir',
+    vazirMedium: 'Vazir Medium',
+    vazirBold: 'Vazir Bold',
+  },
+  english: {
+    openSansRegular: 'OpenSans-Regular',
+  },
+}
